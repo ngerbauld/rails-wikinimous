0ec6aa9ae0989467e2a5c2674d03bd54a089b71c
nicole josquin <nicole.gerbauld@gmail.com>
2019-07-23 18:33:22 +0800

    articles page done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,38 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   def new
+    @article = Article.new
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
