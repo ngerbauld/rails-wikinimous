eb74eb97a4b2ff9f78b29eefb959cfb86c8e7d4d
nicole josquin <nicole.gerbauld@gmail.com>
2019-07-23 16:25:25 +0800

    articles pages done with css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'articles/create'
-  get 'articles/destroy'
-  get 'articles/new'
-  get 'articles/update'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
