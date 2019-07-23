6db0864171a86785156da11c917b5eda4c3cef45
nicole josquin <nicole.gerbauld@gmail.com>
2019-07-23 15:55:14 +0800

    Added 7 views files and controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def update
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+10.times do
+  new_article = Article.new(
+    title: Faker::Name.name,
+    content: Faker::Lorem.paragraph
+  )
+  new_article.save
+end
